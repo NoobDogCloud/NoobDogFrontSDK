ada0d6a39edec35d448b0a80facf9cf48a852924
putao520 <yuyao1022@hotmail.com>
2022-07-14 13:20:57 +0800

    增加压缩JSON方法

---
+++ gfw/util.d.ts
@@ -0,0 +1,5 @@
+export declare function toObject(v : any): object;
+export declare function toString(v : any): string;
+export declare function invalidate(v : any): boolean;
+export declare function invalidateAll(v : any): boolean;
+export declare function compress(t: number, e: string): string;
--- gfw/util.js
+++ gfw/util.js
@@ -15,3 +15,24 @@ export function invalidate(v){
 export function invalidateAll(v){
     return _.isUndefined(v) || _.isNull(v) || '' || 0 || false;
 }
+
+// t = 1 -> json压缩
+// t = 2 -> 转义
+// t = 4 -> 去除转义
+export function compress(t, e) {
+    if (1 === t || 3 === t) {
+        let n = [];
+        let o = 0;
+        let r = (e = e.split("\n").join(" ")).length;
+        let i = !1;
+        for (; o < r; o++) {
+            let a = e.charAt(o);
+            i && a === i ? "\\" !== e.charAt(o - 1) && (i = !1) : i || '"' !== a && "'" !== a ? i || " " !== a && "\t" !== a || (a = "") : i = a,
+                n.push(a)
+        }
+        e = n.join("")
+    }
+    2 !== t && 3 !== t || (e = e.replace(/\\/g, "\\\\").replace(/\"/g, '\\"')),
+    4 === t && (e = e.replace(/\\\\/g, "\\").replace(/\\\"/g, '"'));
+    return e
+}
